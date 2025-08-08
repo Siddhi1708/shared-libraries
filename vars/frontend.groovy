5c82de52bac3a55de18027049950040bc55cdd04
Siddhi1708 <siddhikarpe15543@gmail.com>
2025-08-08 17:06:27 +0530

    Update frontend.groovy

--- vars/frontend.groovy
+++ vars/frontend.groovy
@@ -1,9 +1,19 @@
-def call(String ProjectName,String ImageTag,String DockerHubUser)
-{
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag}"
-  withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-        echo "Logging in to Docker Hub as ${dockerHubUser}"
+def call(String projectName, String imageTag, String dockerHubUser, String credentialsId) {
+
+    echo "Building Frontend Docker image: ${dockerHubUser}/${projectName}:${imageTag}"
+
+    sh "docker build -t ${dockerHubUser}/${projectName}:${imageTag} ."
+ 
+    withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+
+        echo "Logging in to Docker Hub as ${DOCKER_USER}"
+
         sh 'echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin'
-        echo "Pushing Docker image to Docker Hub"
-        sh "docker push ${dockerHubUser}/${projectName}:${imageTag}
+ 
+        echo "Pushing Frontend image to Docker Hub"
+
+        sh "docker push ${dockerHubUser}/${projectName}:${imageTag}"
+
+    }
+
 }
