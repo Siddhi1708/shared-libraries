0d910b132ad0a0c1e3896c840fb81ba8ed113cf1
Siddhi1708 <siddhikarpe15543@gmail.com>
2025-08-08 17:29:35 +0530

    Update frontend.groovy

--- vars/frontend.groovy
+++ vars/frontend.groovy
@@ -1,19 +1,10 @@
-def call(String projectName, String imageTag, String dockerHubUser, String credentialsId) {
-
-    echo "Building Frontend Docker image: ${dockerHubUser}/${projectName}:${imageTag}"
-
-    sh "docker build -t ${dockerHubUser}/${projectName}:${imageTag} ."
- 
-    withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-
-        echo "Logging in to Docker Hub as ${DOCKER_USER}"
-
-        sh 'echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin'
- 
-        echo "Pushing Frontend image to Docker Hub"
-
-        sh "docker push ${dockerHubUser}/${projectName}:${imageTag}"
-
+def call(String dockerhubCreds, String frontendImage, String frontendDir) {
+    stage('Build & Push Frontend Image') {
+        dir(frontendDir) {
+            withDockerRegistry(credentialsId: dockerhubCreds, url: '') {
+                sh "docker build -t ${frontendImage} ."
+                sh "docker push ${frontendImage}"
+            }
+        }
     }
-
 }
