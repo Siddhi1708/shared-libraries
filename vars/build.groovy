ca322360e6395aa5267239443ea1f740f34504b4
Siddhi1708 <siddhikarpe15543@gmail.com>
2025-08-08 17:29:02 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,21 +1,8 @@
-def call(String projectName, String imageTag, String dockerHubUser, String credentialsId) {
-
-    echo "Building Docker image: ${dockerHubUser}/${projectName}:${imageTag}"
-
-    sh "docker build -t ${dockerHubUser}/${projectName}:${imageTag} ."
- 
-    withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-
-        echo "Logging in to Docker Hub as ${DOCKER_USER}"
-
-        sh 'echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin'
- 
-        echo "Pushing Docker image to Docker Hub"
-
-        sh "docker push ${dockerHubUser}/${projectName}:${imageTag}"
-
+def call(String dockerhubCreds, String backendImage) {
+    stage('Build & Push Backend Image') {
+        withDockerRegistry(credentialsId: dockerhubCreds, url: '') {
+            sh "docker build -t ${backendImage} ."
+            sh "docker push ${backendImage}"
+        }
     }
-
 }
-
- 
